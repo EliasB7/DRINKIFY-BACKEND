60cf97a1daf5ae0e7f1e0592f9583a8a29fbbd96
EliasB7 <113938831+EliasB7@users.noreply.github.com>
2023-10-29 20:07:35 -0300

    some deploy solutions

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,16 @@ import * as dotenv from 'dotenv';
 
 async function bootstrap() {
   dotenv.config();
+  const PORT = process.env.PORT || 8080;
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(8080);
+
+  try {
+    await app.listen(PORT);
+    console.log(`Server is running on port ${PORT}`);
+  } catch (error) {
+    console.error(`Error starting the server: ${error}`);
+  }
 }
 
 bootstrap();
